0cbfd2b2fdc13bf77365df2b6162f43c33ac4ab8
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-09-10 22:40:34 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import './App.css';
+import Counter from './components/Counter';
+import Button from './components/Button';
+
+
+function App() {
+
+  const [count, setCount] = useState(0);
+
+
+
+  return (
+    <div className="App">
+      <Counter count={count} />
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
